792f85adb7d6afb31fe384b9e01fd752cffd5827
段佳维 <554343346@qq.com>
2019-04-23 19:25:26 +0800

    hutool工具包

---
+++ springboot-hutool/src/test/java/HutoolTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 工具包部分测试，更多请见hutool官网文档：https://hutool.cn
+ *
+ * Hutool是一个Java工具包类库，对文件、流、加密解密、转码、正则、线程、XML等JDK方法进行封装，组成各种Util工具类
+ */
+@RunWith(SpringRunner.class)
+public class HutoolTest {
+    @Test
+    public void stringTest(){
+
+    }
+}
