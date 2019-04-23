aa86d94f10b7db94216b946f90085a556abd89b6
554343346@qq.com <duan123>
2019-04-23 22:54:26 +0800

    添加bug代码

---
+++ simple-spring-boot-starter/src/main/java/com/duan/springboot/learning/starter/service/ExampleAutoConfigure.java
@@ -0,0 +1,30 @@
+package com.duan.springboot.learning.starter.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(ExampleService.class)
+@EnableConfigurationProperties(ExampleServiceProperties.class)
+public class ExampleAutoConfigure {
+
+    private final ExampleServiceProperties properties;
+
+    @Autowired
+    public ExampleAutoConfigure(ExampleServiceProperties properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = "example.service", value = "enabled",havingValue = "true")
+    ExampleService exampleService (){
+        return  new ExampleService(properties.getPrefix(),properties.getSuffix());
+    }
+
+}
---
+++ simple-spring-boot-starter/src/main/java/com/duan/springboot/learning/starter/service/ExampleService.java
@@ -0,0 +1,14 @@
+package com.duan.springboot.learning.starter.service;
+public class ExampleService {
+
+    private String prefix;
+    private String suffix;
+
+    public ExampleService(String prefix, String suffix) {
+        this.prefix = prefix;
+        this.suffix = suffix;
+    }
+    public String wrap(String word) {
+        return prefix + word + suffix;
+    }
+}
