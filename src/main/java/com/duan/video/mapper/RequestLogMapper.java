618b56c22160059fbc8e92756f7891ff897c214e
段佳维 <554343346@qq.com>
2019-05-20 12:05:26 +0800

    添加aop请求日志 删除前端代码，跳转到最新的前端

---
+++ src/main/java/com/duan/video/mapper/RequestLogMapper.java
@@ -0,0 +1,12 @@
+package com.duan.video.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.duan.video.pojo.entity.RequestLog;
+
+/**
+ * 请求日志mapper
+ *
+ * @author duanjw
+ */
+public interface RequestLogMapper extends BaseMapper<RequestLog> {
+}
